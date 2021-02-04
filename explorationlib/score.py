d4bfd4d7f21afeb6001523097a0c5b406d6cf99e
Erik Peterson <Erik.Exists@gmail.com>
2021-02-04 13:31:15 -0800

    add first_reward; update avg and total_r to return values for each experiment

--- explorationlib/score.py
+++ explorationlib/score.py
@@ -5,16 +5,70 @@ from explorationlib.util import select_exp
 
 def average_reward(exp_data):
     """Average targets found"""
+
     target_name = "exp_reward"
-    reward = np.asarray(exp_data[target_name])
-    return np.mean(reward)
+
+    averages = []
+    for n in range(exp_data["num_experiments"]):
+        # Get one experiments data
+        sel_data = select_exp(exp_data, n)
+        rewards = sel_data[target_name]
+        # Get stat
+        averages.append(np.mean(rewards))
+
+    return averages
 
 
 def total_reward(exp_data):
     """Total targets found"""
+
     target_name = "exp_reward"
-    reward = np.asarray(exp_data[target_name])
-    return np.sum(reward)
+
+    totals = []
+    for n in range(exp_data["num_experiments"]):
+        # Get one experiments data
+        sel_data = select_exp(exp_data, n)
+        rewards = sel_data[target_name]
+        # Get stat
+        totals.append(np.sum(rewards))
+
+    return totals
+
+
+def first_reward(exp_data):
+    """Find the inverse time to first target.
+    
+    Citation
+    --------
+    Bénichou, O., Loverdo, C., Moreau, M. & Voituriez, R. Intermittent 
+    search strategies. Rev. Mod. Phys. 83, 81–129 (2011).
+    """
+
+    length_name = "agent_step"
+    target_name = "exp_reward"
+
+    firsts = []
+    for n in range(exp_data["num_experiments"]):
+        # Get one experiments data
+        sel_data = select_exp(exp_data, n)
+        rewards = sel_data[target_name]
+        steps = sel_data[length_name]
+
+        # Short circuit if no targets...
+        if np.isclose(np.sum(rewards), 0.0):
+            firsts.append(0.0)
+            continue
+
+        # Time it takes to find the
+        # first target/reward
+        time = 1
+        for r, l in zip(rewards, steps):
+            if np.abs(r) > 0:
+                firsts.append(1 / time)
+            else:
+                time += l
+
+        return firsts
 
 
 def search_efficiency(exp_data):
