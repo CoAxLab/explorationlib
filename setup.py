f2a30d67621c49290ea74f5e9a530e1b1888f4b9
Gabriel Sarch <gabrielsarch@gmail.com>
2023-08-10 14:30:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,36 +8,5 @@ setup(name='explorationlib',
       author='Erik J. Peterson',
       author_email='erik.exists@gmail.com',
       license='GPL3',
-      packages=['explorationlib'],
+      packages=['explorationlib', 'ADMCode', 'ADMCode.snuz', 'ADMCode.snuz.ars', 'ADMCode.snuz.ppo', 'ADMCode.snuz.ppo.agents'],
       zip_safe=False)
-
-# package_data = {'ADMCode':['notebooks/*.ipynb', 'notebooks/images/*.png', 'notebooks/images/*.mp4', 'data/*.csv']}
-
-major = 0
-minor = 5
-patch = 2
-__version__ = '.'.join([str(v) for v in [major, minor, patch]])
-
-setup(
-    name='ADMCode',
-    version=__version__,
-    author='Kyle Dunovan, Timothy Verstynen',
-    author_email='dunovank@gmail.com',
-    url='http://github.com/CoAxLab/AdaptiveDecisionMaking_2018',
-    packages=['ADMCode', 'ADMCode.snuz', 'ADMCode.snuz.ars', 'ADMCode.snuz.ppo', 'ADMCode.snuz.ppo.agents'],
-    # package_data=package_data,
-    description='Code and lab resources for Neural and Cognitive Models of Adaptive Decision Making course (2018)',
-    install_requires=['numpy', 'scipy', 'pandas', 'matplotlib', 'seaborn', 'scikit-learn', 'numba', 'future'],
-    include_dirs = [np.get_include()],
-    classifiers=[
-                'Environment :: Console',
-                'Operating System :: OS Independent',
-                'License :: OSI Approved :: MIT License',
-                'Development Status :: 3 - Alpha',
-                'Programming Language :: Python',
-                'Programming Language :: Python :: 3',
-                'Programming Language :: Python :: 3.4',
-                'Programming Language :: Python :: 3.6',
-                'Topic :: Scientific/Engineering',
-                ]
-)
