837fe2eba3098c5694cb803d3a01183a139582a6
Gabriel Sarch <gabrielsarch@gmail.com>
2023-08-10 14:40:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import numpy as np
 
 setup(name='explorationlib',
@@ -8,5 +8,5 @@ setup(name='explorationlib',
       author='Erik J. Peterson',
       author_email='erik.exists@gmail.com',
       license='GPL3',
-      packages=['explorationlib', 'ADMCode', 'ADMCode.snuz', 'ADMCode.snuz.ars', 'ADMCode.snuz.ppo', 'ADMCode.snuz.ppo.agents'],
+      packages=find_packages(include=['explorationlib', 'ADMCode', 'ADMCode.snuz', 'ADMCode.snuz.ars', 'ADMCode.snuz.ppo', 'ADMCode.snuz.ppo.agents']),
       zip_safe=False)
